fe8a32b52819dbb8ebbe56116973889ef7b15a26
anoopshakya906-png <anoopshakya906@gmail.com>
2026-02-18 12:10:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const users = [
+  {
+    name: "amisha rathore",
+    pic: "https://i.pinimg.com/736x/cd/9b/1c/cd9b1cf5b96e8300751f952488d6c002.jpg",
+    bio: "silent chaos in a loud world ☕ | not for everyone",
+  },
+  {
+    name: "kiara mehta",
+    pic: "https://i.pinimg.com/736x/1f/2f/85/1f2f856bf3a020ed8ee9ecb3306ae074.jpg",
+    bio: "main character energy 🎬 | coffee > everything ☕",
+  },
+  {
+    name: "isha oberoi",
+    pic: "https://i.pinimg.com/736x/23/48/7e/23487ef1268cfe017047a0640318c0d0.jpg",
+    bio: "walking through dreams in doc martens 🖤 | late night thinker",
+  },
+  {
+    name: "arjun malhotra",
+    pic: "https://i.pinimg.com/736x/44/ab/12/44ab12c0d0a34f0f1234567890abcd12.jpg",
+    bio: "gym. grind. goals 💪 | discipline over motivation",
+  },
+  {
+    name: "tara singh",
+    pic: "https://i.pinimg.com/736x/78/ef/56/78ef56c0d0a34f0f9876543210abcd34.jpg",
+    bio: "aesthetic soul ✨ | sunsets & soft playlists 🎶",
+  },
+  {
+    name: "dev kapoor",
+    pic: "https://i.pinimg.com/736x/90/cd/78/90cd78c0d0a34f0f1122334455abcd56.jpg",
+    bio: "tech geek 💻 | building ideas into reality 🚀",
+  },
+  {
+    name: "meera sharma",
+    pic: "https://i.pinimg.com/736x/11/aa/22/11aa22c0d0a34f0f6677889900abcd78.jpg",
+    bio: "books & beaches 📚🌊 | living one chapter at a time",
+  },
+  {
+    name: "kabir khan",
+    pic: "https://i.pinimg.com/736x/33/bb/44/33bb44c0d0a34f0f5566778899abcd90.jpg",
+    bio: "street photography 📸 | finding stories in silence",
+  }
+];
